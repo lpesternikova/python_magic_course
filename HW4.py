5ee4482401c811b2bd671c4e2f0b0168ac562acb
lpesternikova <lpesternikova@gmail.com>
2024-10-16 10:07:43 +0500

    HW4 done

--- HW4.py
+++ HW4.py
@@ -5,15 +5,17 @@
 # 6782 -> 23
 # 0,56 -> 11
 
-num = float(input())
-result = 0
+num = abs(float(input()))
 
-while num != 0:
-    if num > 0 and num % 1 > 1 :
-        result = result + (num % 10)
-        num = num // 10
-    else: 
-        print("Дробное!") 
-        
+while num % 1 != 0:
+    num = num * 10
+
+num = int(num)
+
+result = 0
+while num > 0:
+    result = result + num % 10
+    num = num // 10
+
 print(result)
 
