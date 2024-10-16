15a972afb81c775d32d6be9c48159e72e3100b67
lpesternikova <lpesternikova@gmail.com>
2024-10-17 00:16:03 +0500

    try to fix No5

--- HW5.py
+++ HW5.py
@@ -4,8 +4,14 @@
 # Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
 
 num = int(input())
+new_num = int(input())
 
-if num % 1 == 0 and num % num == 0:
-    print("Это простое число!")
+if num >= 0 and num <= 100000:
+    while new_num < num:
+        new_num = new_num + 1
+        if num % 1 == 0 and num % num == 0 and num % new_num != 0:
+            print("Это простое число!")
+        else:
+            print("Это составное число!")
 else:
-    print("Это составное число!")
+    print("Это число не подходит!")
